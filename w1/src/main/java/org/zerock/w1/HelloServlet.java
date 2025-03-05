1cb19e9c38de1c67ee84e222561eb1449f8f138e
Sung Young Kim <skim8700@gmail.com>
2025-03-05 11:48:55 +0900

    [javaWeb] 세팅

--- w1/src/main/java/org/zerock/w1/HelloServlet.java
+++ w1/src/main/java/org/zerock/w1/HelloServlet.java
@@ -1,4 +1,4 @@
-package org.zerock.javaweb;
+package org.zerock.w1;
 
 import java.io.*;
 import javax.servlet.http.*;
@@ -18,8 +18,6 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("<h1>" + message + "</h1>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
